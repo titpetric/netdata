c708c382ea34a127878edc311037a23f329902ec
Tit Petric <black@scene-si.org>
2016-04-01 20:59:20 +0000

    move install to build.sh, add cleanup, refactor to use original netdata-installer.sh

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM debian:jessie
 
 RUN DEBIAN_FRONTEND=noninteractive apt-get -qq update
-RUN DEBIAN_FRONTEND=noninteractive apt-get -qq -y install zlib1g-dev gcc make git autoconf autogen automake pkg-config
 
 ADD build.sh /build.sh
 ADD run.sh /run.sh
+
 RUN chmod +x /run.sh /build.sh; sync; sleep 1; /build.sh
 
 WORKDIR /
--- build.sh
+++ build.sh
@@ -1,54 +1,27 @@
 #!/bin/bash
 set -e
-git clone https://github.com/firehol/netdata.git /netdata.git
+
+# install dependencies for build
+
+DEBIAN_FRONTEND=noninteractive apt-get -qq -y install zlib1g-dev gcc make git autoconf autogen automake pkg-config
+
+# fetch netdata
+
+git clone https://github.com/firehol/netdata.git /netdata.git --depth=1
 cd /netdata.git
 
-# add netdata user
+# use the provided installer
 
-getent group netdata > /dev/null || groupadd -r netdata
-getent passwd netdata > /dev/null || useradd -r -g netdata -c netdata -s /sbin/nologin -d / netdata
+./netdata-installer.sh --dont-wait --dont-start-it
 
-# install netdata under usr/local
+# remove build dependencies
 
-NETDATA_PREFIX=/usr/local
+cd /
+rm -rf /netdata.git
+DEBIAN_FRONTEND=noninteractive dpkg -P zlib1g-dev gcc make git autoconf autogen automake pkg-config
 
-# build
+# symlink access log and error log to stdout/stderr
 
-./autogen.sh
-
-./configure \
-    --prefix="${NETDATA_PREFIX}/usr" \
-    --sysconfdir="${NETDATA_PREFIX}/etc" \
-    --localstatedir="${NETDATA_PREFIX}/var" \
-    --with-zlib --with-math --with-user=netdata \
-    CFLAGS="-O3"
-
-# create folders
-
-mkdir -p ${NETDATA_PREFIX}/usr/share/netdata/web
-mkdir -p ${NETDATA_PREFIX}/etc/netdata
-mkdir -p ${NETDATA_PREFIX}/var/cache/netdata
-mkdir -p ${NETDATA_PREFIX}/var/log/netdata
-
-touch ${NETDATA_PREFIX}/etc/netdata/netdata.conf
-
-# build
-
-make
-
-# install
-
-make install
-
-# fix permissions
-
-chown -R netdata.netdata ${NETDATA_PREFIX}/usr/share/netdata/web
-chown -R netdata.netdata ${NETDATA_PREFIX}/etc/netdata
-chown -R netdata.netdata ${NETDATA_PREFIX}/var/cache/netdata
-chown -R netdata.netdata ${NETDATA_PREFIX}/var/log/netdata
-
-# some permissions (make install will setuid root to below location)
-
-chown root.root ${NETDATA_PREFIX}/usr/libexec/netdata/plugins.d/apps.plugin
-chmod 4755 ${NETDATA_PREFIX}/usr/libexec/netdata/plugins.d/apps.plugin
-
+ln -sf /dev/stdout /var/log/netdata/access.log
+ln -sf /dev/stdout /var/log/netdata/debug.log
+ln -sf /dev/stderr /var/log/netdata/error.log
