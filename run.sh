29f5e4dafba2944bcc16ffd718fe13a5ce6586ea
Tit Petric <tit.petric@monotek.net>
2016-10-03 13:00:40 +0200

    add chown, comments

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# fix permissions due to netdata running as root
+chown root:root /usr/share/netdata/web/ -R
+
 # set up ssmtp
 if [[ $SSMTP_TO ]] && [[ $SSMTP_USER ]] && [[ $SSMTP_PASS ]]; then
 cat << EOF > /etc/ssmtp/ssmtp.conf
@@ -18,9 +21,11 @@ root:netdata@$SSMTP_HOSTNAME:$SSMTP_SERVER:$SSMTP_PORT
 EOF
 fi
 
+# exec custom command
 if [[ $# -gt 0 ]] ; then
         exec "$@"
         exit
 fi
 
+# main entrypoint
 exec /usr/sbin/netdata -D -u root -s /host -p ${NETDATA_PORT}
