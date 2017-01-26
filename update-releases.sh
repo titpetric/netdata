8cc5a4538f6ac20dfda60c09bdc5b0233d1eaf4b
Tit Petric <black@scene-si.org>
2017-01-26 18:51:53 +0100

    update release script

--- update-releases.sh
+++ update-releases.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
+#
+# It should go without saying, this script is for development/deployment purposes
+# only. If a new tag is added into `firehol/netdata`, this creates the corresponding
+# directory (symlink) structure. Most likely, it should be deleted because the build
+# steps for netdata have and will change over time. What is symlinked today, will
+# not be accurate in the future, as the Dockerfiles are currently maintained only
+# for the master branch. Results may vary.
+#
+# And of course, there's the small issue to add individual tags manually to Docker Hub.
+#
 if [ ! -f "update-releases.json" ]; then
 	curl -s https://api.github.com/repos/firehol/netdata/tags > update-releases.json
 fi
-cat update-releases.json | jq -r ".[].name" | egrep 'v[1-9].+[0-9]$' | xargs -n1 -I{} echo mkdir releases/{} \; ln -s ../latest/Dockerfile releases/{}/Dockerfile \; echo {} \> releases/{}/git-tag | sh -x
+TAGS=$(cat update-releases.json | jq -r ".[].name" | egrep 'v[1-9].+[0-9]$')
+for TAG in $TAGS; do
+	if [ ! -d "releases/$TAG" ]; then
+		echo "Creating tag: $TAG"
+		rsync -a releases/latest/ releases/$TAG/
+		echo $TAG > releases/$TAG/git-tag
+	else
+		# tags are sorted, don't create older than the latest created tag
+		exit
+	fi
+done
